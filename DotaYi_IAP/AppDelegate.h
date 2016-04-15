86119d644332c0e64891a758a57bd3ad898d4ccf
Nss2014 <bm465681651@qq.com>
2016-04-15 12:16:08 +0800

    Initial Commit

---
+++ DotaYi_IAP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DotaYi_IAP
+//
+//  Created by 牛松松 on 16/4/15.
+//  Copyright © 2016年 牛松松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DotaYi_IAP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DotaYi_IAP
+//
+//  Created by 牛松松 on 16/4/15.
+//  Copyright © 2016年 牛松松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
